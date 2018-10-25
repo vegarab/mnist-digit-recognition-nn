07557ed7ec1f6d29db09ca96d2b7bb10b2d587cd
Vegar Andreas Bergum <vegarab@stud.ntnu.no>
2018-10-25 21:08:53 +0200

    Initial NN structure. 97% accuracy

---
+++ src/mnist.py
@@ -0,0 +1,34 @@
+import keras
+from keras.datasets import mnist
+from keras import models
+from keras import layers
+from keras.utils import to_categorical
+
+def preprocess_dataset():
+    """Gather and process mnist-dataset"""
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+    network = models.Sequential()
+    network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
+    network.add(layers.Dense(10, activation='softmax'))
+
+    network.compile(optimizer='rmsprop',
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'])
+
+    train_images = train_images.reshape((60000, 28 * 28))
+    train_images = train_images.astype('float32') / 255
+
+    test_images = test_images.reshape((10000, 28 * 28))
+    test_images = test_images.astype('float32') / 255
+
+    train_labels = to_categorical(train_labels)
+    test_labels = to_categorical(test_labels)
+
+    network.fit(train_images, train_labels, epochs=5, batch_size=128)
+
+    test_loss, test_acc = network.evaluate(test_images, test_labels)
+    print('test_acc:', test_acc)
+
+if __name__ == '__main__':
+    preprocess_dataset()
