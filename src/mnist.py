faedc2ac5afffe829900b05060462b377c172e5a
Vegar Andreas Bergum <vegarab@stud.ntnu.no>
2018-11-01 20:57:20 +0100

    adds visualization. 97% accuracy, no overfitting?

--- src/mnist.py
+++ src/mnist.py
@@ -4,6 +4,8 @@ from keras import models
 from keras import layers
 from keras.utils import to_categorical
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def preprocess_dataset():
     """Gather and process mnist-dataset"""
@@ -15,14 +17,12 @@ def preprocess_dataset():
     train_images = (train_images - pixel_mean) / pixel_std
     test_images = (test_images - pixel_mean) / pixel_std
 
-    # reshape (60000, 28, 28) data to a linear vector
-    train_images = train_images.reshape(train_images.shape[0], 28*28)
-    test_images = test_images.reshape(test_images.shape[0], 28*28)
-    train_images = train_images.astype('float32')
-    test_images = test_images.astype('float32')
-
     train_labels = to_categorical(train_labels)
     test_labels = to_categorical(test_labels)
+
+    # reshape (60000, 28, 28) to (60000, 28, 28, 1)
+    train_images = train_images[:,:,:, np.newaxis].astype(np.float32)
+    test_images = test_images[:,:,:, np.newaxis].astype(np.float32)
 
     return train_images, train_labels, test_images, test_labels
 
@@ -30,21 +30,52 @@ def setup_train_network(train_images, train_labels, test_images, test_labels):
     """Setup neural network layers and fit the model"""
     network = models.Sequential()
 
-    network.add(layers.Dense(output_dim = 256, input_dim = train_images.shape[1], activation='relu'))
-    network.add(layers.Dense(256, activation='relu'))
+    network.add(layers.Flatten(input_shape=train_images.shape[1:]))
+    network.add(layers.Dense(196, activation='relu'))
     network.add(layers.Dense(10, activation='softmax'))
 
-    network.compile(optimizer='rmsprop',
+    network.compile(optimizer='adam',
                     loss='categorical_crossentropy',
                     metrics=['accuracy'])
 
     network.summary()
-    network.fit(train_images, train_labels, epochs=10, batch_size=128, validation_split = 0.25)
+    network.fit(train_images, 
+                train_labels, 
+                epochs=8, 
+                batch_size=128, 
+                validation_split = 0.25)
 
     test_loss, test_acc = network.evaluate(test_images, test_labels)
     print('test_acc:', test_acc)
 
+    ### ---- VISUALIZATION ---- ###
+    history = network.history.history
+
+    # plot losses
+    plt.figure(figsize=(12, 8))
+    plt.plot(history["val_loss"], label="Validation loss")
+    plt.plot(history["loss"], label="Training loss")
+    plt.show()
+
+    # plot accuracy
+    plt.figure(figsize=(12, 8))
+    plt.plot(history["val_acc"], label="Validation accuracy")
+    plt.plot(history["acc"], label="Training accuracy")
+    plt.show()
+
+    final_loss, final_accuracy = network.evaluate(test_images, test_labels)
+    print("The final loss on the test set is:", final_loss)
+    print("The final accuracy on the test set is:", final_accuracy)
+
+    # plot final losses
+    plt.figure(figsize=(12, 8))
+    plt.plot(history["val_loss"], label="Validation loss")
+    plt.plot(history["loss"], label="Training loss")
+    plt.plot([7], [final_loss], 'o', label="Final test loss")
+    plt.show()
+
 
 if __name__ == '__main__':
     train_images, train_labels, test_images, test_labels = preprocess_dataset()
-    setup_train_network(train_images, train_labels, test_images, test_labels)
+    history = setup_train_network(train_images, train_labels, 
+                test_images, test_labels)
